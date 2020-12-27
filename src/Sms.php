b12b5fa1322b4f85066dc236bc015b1cee146a49
ditibal <ditibal@gmail.com>
2020-12-27 12:25:36 +0300

    init

--- src/Sms.php
+++ src/Sms.php
@@ -5,6 +5,7 @@ namespace ditibal\smssender;
 use Yii;
 use yii\base\Component;
 use ditibal\smssender\queue\SendSmsJob;
+use yii\base\InvalidArgumentException;
 
 class Sms extends Component
 {
@@ -12,11 +13,34 @@ class Sms extends Component
     public $phone;
     public $message;
     private $_async;
+    private $pushDelay;
 
+
+    private function clearPhone($phone)
+    {
+        $phone = preg_replace('/[^0-9]/', '', $phone);
+
+        if (strlen($phone) == 11 && $phone[0] == 8) {
+            $phone[0] = '7';
+        }
+
+        if (strlen($phone) == 10) {
+            $phone = '7' . $phone;
+        }
+
+        return $phone;
+    }
 
     public function setPhone($phone)
     {
+        $phone = $this->clearPhone($phone);
+
+        if (!preg_match('/\d{11}/', $phone)) {
+            throw new InvalidArgumentException('Phone is invalid.');
+        }
+
         $this->phone = $phone;
+
         return $this;
     }
 
@@ -32,12 +56,21 @@ class Sms extends Component
         return $this;
     }
 
+    public function delay($value)
+    {
+        $this->pushDelay = $value;
+        return $this;
+    }
+
     public function send()
     {
         if ($this->sender->queue && !$this->_async) {
-            $this->sender->queue->push(new SendSmsJob([
-                'sms' => $this,
-            ]));
+            $this->sender
+                ->queue
+                ->delay($this->pushDelay)
+                ->push(new SendSmsJob([
+                    'sms' => $this,
+                ]));
 
             return true;
         }
